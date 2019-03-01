c2657e5e007440095b0baf251fe2a22a7625f862
TheKayneGame <Kayne.saridjo@gmail.com>
2019-03-01 09:51:45 +0100

    Update - Working? needs to be tested

--- vhdl_code/GPU/sources/ScanlineOAM.vhd
+++ vhdl_code/GPU/sources/ScanlineOAM.vhd
@@ -1,21 +1,21 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
+-- Company:
+-- Engineer:
+--
 -- Create Date: 02/28/2019 11:50:37 AM
--- Design Name: 
+-- Design Name:
 -- Module Name: ScanlineOAM - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
 
@@ -32,12 +32,34 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity ScanlineOAM is
-    Port ( OAMin : in STD_LOGIC_VECTOR (31 downto 0));
+    Port (
+    clk : in STD_LOGIC;
+
+    --scanline
+    scanline : in STD_LOGIC_VECTOR (9 downto 0);
+
+    --Object Attribute Memory
+    OAMim : in STD_LOGIC_VECTOR (31 downto 0);
+    OAMadd : out STD_LOGIC_VECTOR (6 downto 0);
+    
+    --Secondary Object Attribute Memory
+    SOAMout : out STD_LOGIC_VECTOR (31 downto 0);
+    SOAMpush : out STD_LOGIC;
+    SOAMfull : in STD_LOGIC
+    );
 end ScanlineOAM;
 
 architecture Behavioral of ScanlineOAM is
+    signal objectIndex : STD_LOGIC_VECTOR (6 downto 0);
+    begin
 
-begin
+        process(clk)
+
+        begin
+            if (rising_edge(clk)) then
+
+            end if;
+        end process;
 
 
-end Behavioral;
+    end Behavioral;

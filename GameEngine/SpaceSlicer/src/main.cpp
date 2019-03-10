6a44caebc356607bcebc9c6c190b5b11078323eb
Harmey <hammiekaaz@gmail.com>
2019-03-10 17:21:35 +0100

    Update - Controllers - no real improvement

--- GameEngine/SpaceSlicer/src/main.cpp
+++ GameEngine/SpaceSlicer/src/main.cpp
@@ -28,45 +28,29 @@ extern "C" {
 extern gpio_dev_t GPIO;
 
 esp_err_t controllerInput()
-{
+{	
+	esp_err_t ret;
 	//---------------------------------------------------------------------------
-    i2c_config_t conf;
-    conf.mode = I2C_MODE_MASTER;
-
-    conf.sda_io_num = GPIO_NUM_21;
-    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-
-    conf.scl_io_num = GPIO_NUM_22;
-    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-
-    conf.master.clk_speed = 10;
-    i2c_param_config(I2C_NUM_0, &conf);
-    i2c_driver_install(I2C_NUM_0, conf.mode, 0, 0, 0);
-	//---------------------------------------------------------------------------
-	
-	//---------------------------------------------------------------------------
-    uint8_t* data_rd = (uint8_t*) malloc(512);
+    uint8_t* data_rd = new uint8_t();
 
 	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
 
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, ( 0x07 << 1 ) | I2C_MASTER_READ, I2C_MASTER_ACK);
-	i2c_master_read(cmd, data_rd, 511, I2C_MASTER_ACK);
-	i2c_master_read(cmd, data_rd, 512, I2C_MASTER_NACK);
-    i2c_master_stop(cmd);
+    ret = i2c_master_stop(cmd);
+		printf("errorStop: %s\t\n", esp_err_to_name(ret) );
+    ret = i2c_master_write_byte(cmd, 0b0, 0x1);
+		printf("errorWrite: %s\t\n", esp_err_to_name(ret) );
+	ret = i2c_master_read_byte(cmd, data_rd, I2C_MASTER_NACK);
+		printf("errorRead: %s\t\n", esp_err_to_name(ret) );
+	ret = i2c_master_start(cmd);
+		printf("errorStart: %s\t\n", esp_err_to_name(ret) );
 
+    ret = i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000 / portTICK_RATE_MS);
+		printf("errorBegin: %s\t\n", esp_err_to_name(ret) );
 
-    esp_err_t ret = i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000 / portTICK_RATE_MS);
+	printf("Data: %d\t\n", *data_rd);
 
-    if (ret != ESP_OK) 
-	{
-		return ret;
-    }
-
-	printf("Data: %d", *data_rd);
-
-	delete data_rd;
     i2c_cmd_link_delete(cmd);
+	delete data_rd;
     return ret;
 	//---------------------------------------------------------------------------
 }
@@ -98,11 +82,35 @@ void app_main(void)
     // GPIO.enable    |= 0x060F0034; //enable all needed outputs outputs
     // //xTaskCreate(&doTask, "fukcmedaddy", 4096, (void*) 1, tskIDLE_PRIORITY, &xHandle);
     // xTaskCreate(&doTask, "fukcmedaddy", 8192, (void*) 1, tskIDLE_PRIORITY, &xHandle);
-	while (1)
-	{
-		esp_err_t ret = controllerInput();
-		printf("!!!!!!!!!!!!!!=%s=!!!!!!!!!!!!!!", esp_err_to_name(ret) );
-	}
+	
+	gpio_set_direction(GPIO_NUM_21, GPIO_MODE_INPUT_OUTPUT);
+	gpio_set_direction(GPIO_NUM_22, GPIO_MODE_INPUT_OUTPUT);
+
+	gpio_set_pull_mode(GPIO_NUM_21, GPIO_PULLUP_ONLY);
+	gpio_set_pull_mode(GPIO_NUM_22, GPIO_PULLUP_ONLY);
+
+	//---------------------------------------------------------------------------
+    i2c_config_t conf;
+	esp_err_t ret;
+ 
+    conf.mode = I2C_MODE_MASTER;
+
+    conf.sda_io_num = GPIO_NUM_18;
+    conf.sda_pullup_en = GPIO_PULLUP_DISABLE;
+
+    conf.scl_io_num = GPIO_NUM_19;
+    conf.scl_pullup_en = GPIO_PULLUP_DISABLE;
+
+    conf.master.clk_speed = 100000;
+
+    ret = i2c_param_config(I2C_NUM_0, &conf);
+	printf("errorConfig: %s\n", esp_err_to_name(ret) );
+
+    ret = i2c_driver_install(I2C_NUM_0, conf.mode, 0, 0, 0);
+	printf("errorInstall: %s\n", esp_err_to_name(ret) );
+	//---------------------------------------------------------------------------
+	
+	ret = controllerInput();	
 }
 
 

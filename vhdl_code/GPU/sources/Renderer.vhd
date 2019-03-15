b9b40de65d9cf1c674c7aea50201b94adcef2ef1
Dion Legierse <dal.legierse@student.avans.nl>
2019-03-15 22:48:41 +0100

    Update - Only pixel writer is needed

--- vhdl_code/GPU/sources/Renderer.vhd
+++ vhdl_code/GPU/sources/Renderer.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.std_logic_signed.all;
 
 entity Renderer is
 port(
@@ -34,6 +35,9 @@ signal ScanLineCounter : unsigned(8 downto 0) := b"1_1011_1111";
 signal SOAMAddressCounter : unsigned(4 downto 0) := (others => '0');
 
 signal CurrentSprite : std_logic_vector(31 downto 0) := (others => '0');
+
+signal SpriteY : unsigned(2 downto 0) := (others => '1');
+signal SpriteOffset : unsigned(15 downto 0) := (others => '1');
 
 begin
 	X_Counter : process(clk)
@@ -102,34 +106,107 @@ begin
 	Object_Picker : process(clk)
 	begin
 		if rising_edge(clk) then
-			if SOAMData(0)(22 downto 14) >= xCounter and (SOAMData(0)(22 downto 14) + 8) < xCounter then
+			if isBufferReady then
+				if unsigned(SOAMBuffer(0)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(0)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(0);
+				elsif unsigned(SOAMBuffer(1)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(1)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(1);
+				elsif unsigned(SOAMBuffer(2)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(2)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(2);
+				elsif unsigned(SOAMBuffer(3)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(3)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(3);
+				elsif unsigned(SOAMBuffer(4)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(4)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(4);
+				elsif unsigned(SOAMBuffer(5)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(5)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(5);
+				elsif unsigned(SOAMBuffer(6)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(6)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(6);
+				elsif unsigned(SOAMBuffer(7)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(7)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(7);
+				elsif unsigned(SOAMBuffer(8)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(8)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(8);
+				elsif unsigned(SOAMBuffer(9)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(9)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(9);
+				elsif unsigned(SOAMBuffer(10)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(10)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(10);
+				elsif unsigned(SOAMBuffer(11)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(11)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(11);
+				elsif unsigned(SOAMBuffer(12)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(12)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(12);
+				elsif unsigned(SOAMBuffer(13)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(13)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(13);
+				elsif unsigned(SOAMBuffer(14)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(14)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(14);
+				elsif unsigned(SOAMBuffer(15)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(15)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(15);
+				elsif unsigned(SOAMBuffer(16)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(16)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(16);
+				elsif unsigned(SOAMBuffer(17)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(17)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(17);
+				elsif unsigned(SOAMBuffer(18)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(18)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(18);
+				elsif unsigned(SOAMBuffer(19)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(19)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(19);
+				elsif unsigned(SOAMBuffer(20)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(20)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(20);
+				elsif unsigned(SOAMBuffer(21)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(21)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(21);
+				elsif unsigned(SOAMBuffer(22)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(22)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(22);
+				elsif unsigned(SOAMBuffer(23)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(23)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(23);
+				elsif unsigned(SOAMBuffer(24)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(24)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(24);
+				elsif unsigned(SOAMBuffer(25)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(25)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(25);
+				elsif unsigned(SOAMBuffer(26)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(26)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(26);
+				elsif unsigned(SOAMBuffer(27)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(27)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(27);
+				elsif unsigned(SOAMBuffer(28)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(28)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(28);
+				elsif unsigned(SOAMBuffer(29)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(29)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(29);
+				elsif unsigned(SOAMBuffer(30)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(30)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(30);
+				elsif unsigned(SOAMBuffer(31)(22 downto 14)) <= xCounter and (unsigned(SOAMBuffer(31)(22 downto 14)) + 8) > xCounter then
+					CurrentSprite <= SOAMBuffer(31);
+				else
+					CurrentSprite <= (others => '1');
+				end if;
+			end if;
+		end if;
+	end process;
 
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(2)(22 downto 14) >= xCounter and (SOAMData(2)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(3)(22 downto 14) >= xCounter and (SOAMData(3)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(4)(22 downto 14) >= xCounter and (SOAMData(4)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(5)(22 downto 14) >= xCounter and (SOAMData(5)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(6)(22 downto 14) >= xCounter and (SOAMData(6)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(7)(22 downto 14) >= xCounter and (SOAMData(7)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(8)(22 downto 14) >= xCounter and (SOAMData(8)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(9)(22 downto 14) >= xCounter and (SOAMData(9)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(10)(22 downto 14) >= xCounter and (SOAMData(10)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
-			elsif SOAMData(1)(22 downto 14) >= xCounter and (SOAMData(1)(22 downto 14) + 8) < xCounter then
+	Addres_Calculator : process(clk)
+	variable tempX : unsigned(9 downto 0);
+	variable tempY : unsigned(8 downto 0);
+	begin
+		if rising_edge(clk) then
+			if CurrentSprite /= x"FFFFFFFF" then
+				tempX := (xCounter - 1) - unsigned(CurrentSprite(22 downto 14));
+				tempY := ScanLineCounter - unsigned(CurrentSprite(31 downto 23));
+
+				SpriteY <= tempY(2 downto 0);
+				SpriteOffset <= tempX(2 downto 0) + (unsigned(CurrentSprite(7 downto 0)) * 64);
+			else
+				SpriteY <= (others => '1');
+				SpriteOffset <= (others => '1');
+			end if;
+
+			if SpriteOffset = x"FFFF" then
+				SpriteROMAddr <= (others => '1');
+			else
+				SpriteROMAddr <= std_logic_vector(SpriteOffset + (8 * ('0' & SpriteY)));
+			end if;
+
+		end if;
+	end process;
+
+	Pixel_Writer : process(clk)
+	begin
+		if rising_edge(clk) then
+			if isBufferReady and xCounter >= 4 then
 
 			end if;
 		end if;
--- vhdl_code/GPU/sources/test scripts/Renderer.do
+++ vhdl_code/GPU/sources/test scripts/Renderer.do
@@ -1,16 +1,22 @@
 restart -f
 force clk 0 0 ns, 1 5 ns -r 10 ns
-force SOAMData 2#0000010000010000000000000000000
-force SpriteRomDatout 16#FE
+force SOAMData 2#0000010000010000000000000000001
+force SpriteRomDatout(0) 0 0 ns, 1 5 ns -r 10 ns
+force SpriteRomDatout(1) 0 0 ns, 1 10 ns -r 20 ns
+force SpriteRomDatout(2) 0 0 ns, 1 20 ns -r 40 ns
+force SpriteRomDatout(3) 0 0 ns, 1 40 ns -r 80 ns
+force SpriteRomDatout(4) 0 0 ns, 1 80 ns -r 160 ns
+force SpriteRomDatout(5) 0 0 ns, 1 160 ns -r 320 ns
+force SpriteRomDatout(6) 0 0 ns, 1 320 ns -r 640 ns
+force SpriteRomDatout(7) 0 0 ns, 1 640 ns -r 1280 ns
 force bufferFull 0
 force bufferClear 0
 
 run 5200 ns
 
-
 force SOAMData 2#0010000000000000100000000000000
 
-run 2400 ns
+run 300 ns
 
 force bufferFull 1
 
@@ -18,8 +24,8 @@ run 100 ns
 
 force bufferFull 0
 
-run 2600 ns
+run 4700 ns
 
-force SOAMData 2#0000010000010000000000000000000
+force SOAMData 2#0000010000010000000000000000001
 
 run 5200 ns

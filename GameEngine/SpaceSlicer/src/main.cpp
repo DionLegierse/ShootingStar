3a57b592ef243e5c7bbeb1405e31f8665ddcfd1a
Harmey <hammiekaaz@gmail.com>
2019-04-08 10:55:24 +0200

    Update - Fixed timing issue with the tasks of timing

--- GameEngine/SpaceSlicer/src/main.cpp
+++ GameEngine/SpaceSlicer/src/main.cpp
@@ -6,6 +6,8 @@
 #include "GameLoops/TestPC.h"
 #include "Handlers/MutexHandler.h"
 
+#define GAME_SPEED 8
+
 void createMainTask();
 void mainTask(void*);
 void inputTask(void*);
@@ -30,65 +32,70 @@ void createMainTask()
 	TaskHandle_t xHandleInput = NULL;
 	TaskHandle_t xHandleDraw = NULL;
 
-	xTaskCreatePinnedToCore (mainTask,
-							 "MAIN",
-							 4096,
-							 (void*) 1,
-							 tskIDLE_PRIORITY,
-							 &xHandleLoop,
-							 0 );
 
-	xTaskCreatePinnedToCore (inputTask,
-				 			 "INPUT",
-							 4096,
-							 (void*) 1,
-							 9,
-							 &xHandleInput,
-							 1);
-	
-	xTaskCreatePinnedToCore (drawTask,
-							 "DRAW",
-							 4096,
-							 (void*) 1,
-							 tskIDLE_PRIORITY,
-							 &xHandleDraw,
-							 0);
+	//Core 0
+	xTaskCreatePinnedToCore (mainTask, "MAIN", 4096, (void*) 1, 2, &xHandleLoop, 0 );				 
+	xTaskCreatePinnedToCore (drawTask, "DRAW", 4096, (void*) 1, 1, &xHandleDraw, 0);
+
+	//Core 1
+	xTaskCreatePinnedToCore (inputTask, "INPUT", 4096, (void*) 1, 3, &xHandleInput, 1);
 }
 
 void mainTask(void* vParam)
 {
 	test->setup();
+	//printf("SETUP_MAIN\n");
 
 	TickType_t xLastWakeTime;
-	const TickType_t xFrequency = 1;
+	const TickType_t xFrequency = GAME_SPEED;
 
 	xLastWakeTime = xTaskGetTickCount();
 
 	for (;;)
 	{
 		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+
+		//printf("%d - ", xTaskGetTickCount());
 		test->loop();
+		//printf("%d\n", xTaskGetTickCount());
+		//printf("MAIN\n");
 	}
 }
 
 void inputTask(void* vParam)
 {
 	test->setupInput();
-
-	for (;;)
-		test->readInput();
-}
-
-void drawTask(void* vParam)
-{
+	//printf("SETUP_INPUT\n");
+	
 	TickType_t xLastWakeTime;
-	const TickType_t xFrequency = 1;
+	const TickType_t xFrequency = GAME_SPEED;
 
 	xLastWakeTime = xTaskGetTickCount();
 
 	for (;;)
 	{
 		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+
+		//printf("%d - ", xTaskGetTickCount());
+		test->readInput();
+		//printf("%d\n", xTaskGetTickCount());
+		//printf("INPUT\n");
+	}
+}
+
+void drawTask(void* vParam)
+{
+	TickType_t xLastWakeTime;
+	const TickType_t xFrequency = GAME_SPEED;
+
+	xLastWakeTime = xTaskGetTickCount();
+
+	for (;;)
+	{
+		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+
 		test->updateAllSprites();
+		//printf("%d\n", xTaskGetTickCount());
+		//printf("DRAW\n");
 	}
 }
